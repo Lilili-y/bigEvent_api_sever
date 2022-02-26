4c94adf1d40be09fc4c7136611c3ecc0df41d69e
Lilili-y <184972241@qq.com>
2022-02-26 16:57:22 +0800

    获取用户信息，更新用户基本信息

---
+++ router/userinfo.js
@@ -0,0 +1,18 @@
+//导入express
+const express = require('express')
+//创建路由实例对象
+const router = express.Router()
+//导入获取用户信息的处理函数模块
+const userinfo_handler = require('../router_handler/userinfo')
+//导入验证数据合法性的中间件
+const expressJoi = require('@escook/express-joi')
+//导入需要验证的规则对象，通过解构赋值只导入需要的
+const { update_userinfo_schema, update_password_schema } = require('../schema/user')
+//获取用户基本信息
+router.get('/userinfo', userinfo_handler.getUserinfo)
+//更新用户的的基本信息
+router.post('/userinfo', expressJoi(update_userinfo_schema), userinfo_handler.updataUserinfo)
+//重置密码
+router.post('/updatepwd', expressJoi(update_password_schema), userinfo_handler.updatePwd)
+//向外共享路由对象
+module.exports = router 
--- schema/user.js
+++ schema/user.js
@@ -3,10 +3,36 @@ const joi = require('joi')
 //定义用户名和密码的验证规则
 const username = joi.string().alphanum().min(1).max(10).required()
 const password = joi.string().pattern(/^[\S]{6,12}$/).required()
+//定义id,nickname,email的验证规则
+const id = joi.number().integer().min(1).required()
+const nickname = joi.string().required()
+const email = joi.string().email().required()
 //定义验证注册和登录的表单验证规则
 exports.reg_login_schema = {
     body: {
         username,
         password,
     },
+}
+//定义验证更新用户基本信息的表单验证规则
+exports.update_userinfo_schema = {
+    //需要对req.body里面的表单数据进行验证
+    body: {
+        id,
+        nickname,
+        email
+    }
+}
+//定义重置密码的表单验证规则
+exports.update_password_schema = {
+    body: {
+        // 使用 password 这个规则，验证 req.body.oldPwd 的值
+        oldPwd: password,
+        // 使用 joi.not(joi.ref('oldPwd')).concat(password) 规则，验证 req.body.newPwd 的值
+        // 解读：
+        // 1. joi.ref('oldPwd') 表示 newPwd 的值必须和 oldPwd 的值保持一致
+        // 2. joi.not(joi.ref('oldPwd')) 表示 newPwd 的值不能等于 oldPwd 的值
+        // 3. .concat() 用于合并 joi.not(joi.ref('oldPwd')) 和 password 这两条验证规则
+        newPwd: joi.not(joi.ref('oldPwd')).concat(password),
+    }
 }
